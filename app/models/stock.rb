4f565875c4113120aaf3c8969801f6ba148ca39c
iRa <ira.susanto@racerocks3d.com>
2025-03-26 17:41:41 -0700

    Create and display stock objects in browser

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,8 @@
 class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol)
+    company = Alphavantage::Fundamental.new(symbol: ticker_symbol).overview
     quote = Alphavantage::TimeSeries.new(symbol: ticker_symbol).quote
-    quote.price
+
+    new(ticker: ticker_symbol, name: company.name, last_price: quote.price)
   end
 end
