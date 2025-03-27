4e661a4d8468992e4d1cc5c4901b0eb987cfadd5
iRa <ira.susanto@racerocks3d.com>
2025-03-26 17:58:29 -0700

    Dealing with invalid search results

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,8 +1,12 @@
 class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol)
-    company = Alphavantage::Fundamental.new(symbol: ticker_symbol).overview
-    quote = Alphavantage::TimeSeries.new(symbol: ticker_symbol).quote
+    begin
+      company = Alphavantage::Fundamental.new(symbol: ticker_symbol).overview
+      quote = Alphavantage::TimeSeries.new(symbol: ticker_symbol).quote
 
-    new(ticker: ticker_symbol, name: company.name, last_price: quote.price)
+      new(ticker: ticker_symbol, name: company.name, last_price: quote.price)
+    rescue => exception
+      return nil
+    end
   end
 end
