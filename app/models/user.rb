c617c6936f64e1f7202e818cda87da5b73e09f38
iRa <ira.susanto@racerocks3d.com>
2025-03-31 17:29:07 -0700

    Search users/friends: implement search method

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,6 +28,32 @@ class User < ApplicationRecord
 
   def full_name
     return "#{first_name} #{last_name}" if first_name || last_name
-    "Anonymous"
-  end 
+    'Anonymous'
+  end
+
+  def self.search(param)
+    param.strip!
+
+    to_send_back = (first_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq
+
+    return nil unless to_send_back
+
+    to_send_back
+  end
+
+  def self.first_name_matches(param)
+    matches('first_name', param)
+  end
+
+  def self.last_name_matches(param)
+    matches('last_name', param)
+  end
+
+  def self.email_matches(param)
+    matches('email', param)
+  end
+
+  def self.matches(field_name, param)
+    where("#{field_name} like ?", "%#{param}%")
+  end
 end
