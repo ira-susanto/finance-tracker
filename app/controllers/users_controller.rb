af8319e184cc95f8f704b7ea7fd02ee7b01e4984
iRa <ira.susanto@racerocks3d.com>
2025-03-31 17:03:25 -0700

    Create search friends form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,28 @@ class UsersController < ApplicationController
   def my_friends
     @friends = current_user.friends
   end
+
+  def search
+    render json: params[:friend]
+    
+    # if params[:friend].present?
+    #   @stock = Stock.new_lookup(params[:stock])
+
+    #   if @stock
+    #     respond_to do |format|
+    #       format.js { render partial: 'users/result' }
+    #     end
+    #   else
+    #     respond_to do |format|
+    #       flash.now[:alert] = 'Please enter a valid symbol to search'
+    #       format.js { render partial: 'users/result' }
+    #     end
+    #   end
+    # else
+    #   respond_to do |format|
+    #     flash.now[:alert] = 'Please enter a symbol to search'
+    #     format.js { render partial: 'users/result' }
+    #   end
+    # end
+  end
 end
