bdda21b840b5e1ddf3befb44cadce4c0a175c3b5
iRa <ira.susanto@racerocks3d.com>
2025-04-04 08:49:12 -0700

    Display user/friend profile and tracking options

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :user_stocks, only: [:create, :destroy]
-  resources :friendships, only: [:create, :destroy]
   devise_for :users
+  resources :users, only: [ :show ]
+  resources :user_stocks, only: [ :create, :destroy ]
+  resources :friendships, only: [ :create, :destroy ]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
